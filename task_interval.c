844348d787cf14d5eb0aa2f20f8c3c7ad7730a4c
uhziel <uhziel@gmail.com>
2021-04-13 23:02:15 +0800

    添加 perf stat 的使用说明

--- task_interval.c
+++ task_interval.c
@@ -6,6 +6,24 @@ $ sudo perf probe sdt_task_interval:leave_task
 
 $ sudo perf record -e "{sdt_task_interval:enter_task,sdt_task_interval:leave_task}" -g ./task_interval
 $ sudo perf record -e "{sdt_task_interval:enter_task,sdt_task_interval:leave_task}" -ga
+$ sudo perf stat -e "{sdt_task_interval:enter_task,sdt_task_interval:leave_task}" -g ./task_interval # 统计接受到的消息总数
+task 1
+task 2
+task 3
+task 4
+task 5
+task 6
+^C./task_interval: Interrupt
+
+ Performance counter stats for './task_interval':
+
+                 6      sdt_task_interval:enter_task
+                 5      sdt_task_interval:leave_task
+
+       5.334456760 seconds time elapsed
+
+       0.002950000 seconds user
+       0.000000000 seconds sys
  */
 
 #include <sys/sdt.h>
